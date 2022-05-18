7baf577fa5016a9cc4300642b7356b99d37a957e
juvej <juvej@JUV333>
2022-05-18 16:06:38 -0500

    PRIMER CARGA

---
+++ src/test/java/com/JUV3/calculadora/web/CalculadoraWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JUV3.calculadora.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculadoraWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
